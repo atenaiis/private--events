55cd0613f25fed90fc17501513b9c8f4b76b30a5
atenaiis <attennaisc@gmail.com>
2021-04-02 11:39:04 -0600

    Generate model for user and controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+      end
+    
+      def new
+        @user = User.new
+      end
+    
+      def create
+        @user = User.new(user_params)
+        @user.save
+      end
+    
+      def show
+        @user = User.find(params[:id])
+      end
+end
